8ff87239575f09ad54d6e39224c34813d7356e73
Yeuda Shtern <yeudashtern@gmail.com>
2023-09-03 11:25:42 +0300

    one more bug

--- app/api/companion/chat/[chatId]/route.ts
+++ app/api/companion/chat/[chatId]/route.ts
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+
 import { StreamingTextResponse, LangChainStream } from "ai";
 import { auth, currentUser } from "@clerk/nextjs";
 import { Replicate } from "langchain/llms/replicate";
@@ -8,8 +8,6 @@ import { NextResponse } from "next/server";
 import { MemoryManager } from "@/lib/memory";
 import { rateLimit } from "@/lib/rate-limit";
 import prismadb from "@/lib/prismadb";
-
-dotenv.config({ path: `.env` });
 
 export async function POST(
   request: Request,
